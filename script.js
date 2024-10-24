e89cb9a34bd4bae6de4e192fb437f6b6074ad0fd
Franciszek Blachnio <Franciszek.Blacnio@tm1.edu.pl>
2024-10-24 10:08:06 +0200

    Dodawanie poziomu trudnosci

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ let last=-1;
 const boar = document.getElementById("boar");
 const numberDisplay = document.getElementById("number");
 const scoreElement = document.getElementById("score");
+const czasReakcjiP = document.getElementById("czasReakcji");
+const sredniCzasReakcjiP = document.getElementById("sredniCzasReakcji");
+const  = document.
+var startTime = performance.now();
+var czasReakcji = new Array(0, 0);
+
 function updateNumber() {
+    startTime = performance.now();
     number++;
     numberDisplay.textContent = number;
     if (number % 7 === 0 || number.toString().includes('7')) {
@@ -14,14 +21,23 @@ function updateNumber() {
     }
 }
 
+
 setInterval(updateNumber, 1000); 
 
 boar.addEventListener("click", () => {
     if (number % 7 === 0 || number.toString().includes('7')) {
         if (last!=number)
         {
+            var endTime = performance.now();
+            var timeTaken = endTime - startTime;
+            czasReakcji[0] += 1;
+            czasReakcji[1] += timeTaken;
             score+=1;
             scoreElement.textContent=score;
+            //Math.round(timeTaken, 3);
+            //console.log(endTime + ' ' + startTime + ' ' + timeTaken + ' ' + czasReakcji);
+            czasReakcjiP.textContent=Math.round(timeTaken)+'ms';
+            sredniCzasReakcjiP.textContent=Math.round(czasReakcji[1]/czasReakcji[0])+'ms';
         }
 
         last=number;
@@ -40,27 +56,32 @@ boar.addEventListener("click", () => {
     }
     else
     {
-        scoreElement.style.color = "greenyellow";
+        scoreElement.style.color = 'rgb(69, 221, 22)';
+    }
+
+    if (timeTaken>350)    
+    {
+        czasReakcjiP.style.color = "red";
+    }
+    else if (timeTaken>180)
+    {
+        czasReakcjiP.style.color = "gray";
+    }
+    else
+    {
+        czasReakcjiP.style.color = "green";
+    }
+
+    if (czasReakcji[1]/czasReakcji[0]>350)    
+    {
+        sredniCzasReakcjiP.style.color = "red";
+    }
+    else if (czasReakcji[1]/czasReakcji[0]>180)
+    {
+        sredniCzasReakcjiP.style.color = "gray";
+    }
+    else
+    {
+        sredniCzasReakcjiP.style.color = "green";
     }
 });
-
-/*
-\
-// Start timing
-let startTime = performance.now();
-
-// Your code or function to time
-// Example: A loop
-for (let i = 0; i < 1000000; i++) {
-    // Looping
-}
-
-// End timing
-let endTime = performance.now();
-
-// Calculate the time difference
-let timeTaken = endTime - startTime;
-console.log(`Time taken: ${timeTaken} milliseconds`);
-
-
-*/
